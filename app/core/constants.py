68f8d4411ce461c648bbab732984a545711a64ca
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-03 13:02:49 +0330

    Update constants.py

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,1 +1,19 @@
+# app/core/constants.py
+
+APP_NAME = "tgstats"
+DEFAULT_TIMEZONE = "Asia/Tehran"
+
+# Daily report default time (local timezone)
+DEFAULT_REPORT_HOUR = 23
+DEFAULT_REPORT_MINUTE = 30
+
+# Flood detection defaults
+DEFAULT_FLOOD_COUNT = 30           # msgs
+DEFAULT_FLOOD_WINDOW_SECONDS = 60  # seconds
+
+# ML defaults
+ML_MIN_TEXTS = 30
+ML_TOP_PHRASES = 20
+ML_TOPICS = 4
+ML_TOPIC_TERMS = 6
 
